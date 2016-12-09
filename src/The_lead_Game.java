79697cd838aa11a1ce72c38e3467d4f8164e7121
JyotendraPallav <Jyotendra@gmail.com>
2016-12-09 23:49:32 +0530

    Created 1st running version of TLG

--- src/The_lead_Game.java
+++ src/The_lead_Game.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * 
  */
@@ -9,8 +11,29 @@
 public class The_lead_Game {
 
 	public static void main(String[] args) {
-		System.out.println("Printing my code on Eclipse!!");
-
+		System.out.println("Let's play the game!!\n");
+		Scanner sc=new Scanner(System.in);
+		if(sc.hasNextInt()){
+			int No_of_Rounds=sc.nextInt();
+			int Each_Round[][]=new int[No_of_Rounds][4];
+			int Max_Diff=0;
+			int Winner=1;
+			for(int i=0;i<No_of_Rounds;i++){
+				Each_Round[i][0]=sc.nextInt();
+				Each_Round[i][1]=sc.nextInt();
+				Each_Round[i][2]=Each_Round[i][1]-Each_Round[i][0];
+				if(Each_Round[i][2]>0){
+					Each_Round[i][3]=2;
+				}else Each_Round[i][3]=1;
+				if(Math.abs(Each_Round[i][2])>Max_Diff){
+					Max_Diff= Math.abs(Each_Round[i][2]);
+					Winner=Each_Round[i][3];
+				}
+			}
+			for(int i=0;i<No_of_Rounds;i++){
+				System.out.println("Round "+(i+1)+": "+Each_Round[i][0]+" - "+Each_Round[i][1]+" - "+Each_Round[i][2]+" - "+Each_Round[i][3]);
+			}
+			System.out.println("Winner is # "+Winner+" by "+Max_Diff+" margins!");
+		}
 	}
-
 }
