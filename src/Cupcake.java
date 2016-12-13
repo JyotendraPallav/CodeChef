87afd70e5c7db3e4d2571bfd04aeeea5113a22bb
JyotendraPallav <Jyotendra@gmail.com>
2016-12-13 16:29:50 +0530

    Cupcake:updated with a smaller method

--- src/Cupcake.java
+++ src/Cupcake.java
@@ -5,28 +5,35 @@ class Cupcake {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		try{
-			System.out.println("How many test case are there: ");
-			int TestCases=sc.nextInt();
-			int cupcakes[]=new int[TestCases];
-			for(int i=0;i<TestCases;i++){
-				cupcakes[i]=sc.nextInt();
-			}
-			for(int i=0;i<TestCases;i++){
-				System.out.println(package_size(cupcakes[i]));
+			//System.out.println("How many test case are there: ");
+			if(sc.hasNextInt()){
+				int TestCases=sc.nextInt();
+				int cupcakes[]=new int[TestCases];
+				for(int i=0;i<TestCases;i++){
+					cupcakes[i]=sc.nextInt();
+				}
+				for(int i=0;i<TestCases;i++){
+					System.out.println(package_size(cupcakes[i]));
+				}
 			}
 		}finally{
 			sc.close();
 		}
-
+		
 	}
 
 	static int package_size(int tot_cc){
 		int package_size=0;
-		for(int i=2;i<=tot_cc;i++){
-			if((tot_cc%i)>=package_size){ 
-				package_size=tot_cc%i;
-			}
-		}
+		int Temp_size=0;
+		int Diff=0;
+		int i=1;
+		do{
+			Temp_size=tot_cc%(tot_cc-i);
+			Diff=Temp_size-package_size;
+			if(Temp_size>package_size) {package_size=Temp_size;}
+			//System.out.println(Temp_size);
+			i=i+1;
+		}while(Diff>0);
 		if(package_size==0){
 			package_size=tot_cc;
 		}else{
