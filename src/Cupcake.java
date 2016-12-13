2661009453995958e1dabdb455b3af9a86f26229
JyotendraPallav <Jyotendra@gmail.com>
2016-12-13 15:39:35 +0530

    Done cupcakesgit add -A

--- src/Cupcake.java
+++ src/Cupcake.java
@@ -7,11 +7,32 @@ class Cupcake {
 		try{
 			System.out.println("How many test case are there: ");
 			int TestCases=sc.nextInt();
-			int cupcakes[]=new int[5];
+			int cupcakes[]=new int[TestCases];
+			for(int i=0;i<TestCases;i++){
+				cupcakes[i]=sc.nextInt();
+			}
+			for(int i=0;i<TestCases;i++){
+				System.out.println(package_size(cupcakes[i]));
+			}
 		}finally{
 			sc.close();
 		}
 
 	}
 
+	static int package_size(int tot_cc){
+		int package_size=0;
+		for(int i=2;i<=tot_cc;i++){
+			if((tot_cc%i)>=package_size){ 
+				package_size=tot_cc%i;
+			}
+		}
+		if(package_size==0){
+			package_size=tot_cc;
+		}else{
+			package_size=tot_cc-package_size;
+		}
+		return package_size;
+	}
+	
 }
