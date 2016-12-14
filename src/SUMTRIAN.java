fa72274bf572ca81b2a66b1eeb04dfa51a2d8f8f
JyotendraPallav <Jyotendra@gmail.com>
2016-12-15 00:26:28 +0530

    Triangle: Completed and uploaded

--- src/SUMTRIAN.java
+++ src/SUMTRIAN.java
@@ -5,14 +5,14 @@ class SUMTRIAN {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		try{
-			System.out.println("How many numbers of test cases: \n");
+		//	System.out.println("How many numbers of test cases: \t");
 			if(sc.hasNextInt()){
 				
 				int No_of_cases=sc.nextInt();
 				for(int i=0;i<No_of_cases;i++){
-					System.out.println("How many numbers of lines in Triangle: \n");
+			//		System.out.println("How many numbers of lines in Triangle: \t");
 					int No_of_lines=sc.nextInt();
-					int[][] triangle=new int[3][];
+					int[][] triangle=new int[No_of_lines][];
 					for(int row=0; row<No_of_lines;row++){
 						triangle[row]=new int[row+1];
 						for(int col=0;col<=row;col++){
@@ -32,22 +32,28 @@ class SUMTRIAN {
 	}
 
 	static int Find_Path(int[][] SumTrian){
+		//System.out.println(SumTrian.length);
 		int[][] CalTrian=new int[SumTrian.length][];
-		int row,col,i,j;
-		row=col=0;
-		CalTrian[row]=new int[row+1];
-		do while(col<=row){
-			int x=0;
-			for(i=0;i<row;i++){
-				for(j=0;j<=i;j++){
-					int x=x+SumTrian[i][j];
+		CalTrian[0]=new int[1];
+		CalTrian[0][0]=SumTrian[0][0];
+		for(int i=1;i<SumTrian.length;i++){
+			CalTrian[i]=new int[i+1];
+			for(int j=0;j<=i;j++){
+				if(j==0){
+					CalTrian[i][j]=CalTrian[i-1][0]+SumTrian[i][0];
+				}else if(j==i){
+					CalTrian[i][j]=CalTrian[i-1][j-1]+SumTrian[i][j];
+				}else{
+					int left_move=CalTrian[i-1][j-1]+SumTrian[i][j];
+					int bot_move=CalTrian[i-1][j]+SumTrian[i][j];
+					CalTrian[i][j]=Math.max(left_move,bot_move);
 				}
 			}
-			CalTrian[row][col]=x;
 		}
-		
-		
 		int path_value=0;
+		for(int k=0;k<SumTrian.length;k++){
+			path_value=Math.max(path_value, CalTrian[SumTrian.length-1][k]);
+		}
 		return path_value;
 	}
 }
