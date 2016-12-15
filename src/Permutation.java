4237ab5ee409e99cb72e7dde1da4d8e5b1ee3d8e
JyotendraPallav <Jyotendra@gmail.com>
2016-12-15 15:53:40 +0530

    Permutation: Working on

--- src/Permutation.java
+++ src/Permutation.java
@@ -1,9 +1,41 @@
+import java.util.*;
 
 class Permutation {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		try{
+			if(sc.hasNextInt()){
+				System.out.println("Length of permutation?:");
+				int Permu_len=sc.nextInt();
+				int iter=0;
+				List<String> results=new ArrayList<String>();
+				while(Permu_len>0){
+					int[] perm = new int[Permu_len];
+					for(int j=0; j<Permu_len;j++){
+						perm[j]=sc.nextInt();
+					}
+					Permu_len=sc.nextInt();
+					results.add(find_truth(perm));
+					iter=iter+1;
+				};
+				
+			}
+		}finally{
+			sc.close();
+		}
 
+	}
+	
+	static String find_truth(int[] Arr){
+		String verdict="";
+		Int tot_len=Arr.length;
+		while(tot_len>0){
+			for(int i=1;i<=tot_len;i++){
+				if(Arr[i])
+			}
+		}
+		return verdict;
 	}
 
 }
