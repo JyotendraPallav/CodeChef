e87f219b057d550ce63201241b6682c790211eea
JyotendraPallav <Jyotendra@gmail.com>
2016-12-25 16:49:46 +0530

    Transform: Trying out still

--- src/Transform.java
+++ src/Transform.java
@@ -24,8 +24,32 @@ public class Transform {
 	static String Transform_exp(String Expression){
 		String converted="";
 		List<String> Exp=new ArrayList<String>(Arrays.asList(Expression.split("")));
-		System.out.println(Exp.subList(0, Exp.indexOf(")")));
-		System.out.println(Exp);
+		ListIterator<String> litr = null;
+		litr=Exp.listIterator();
+		List<String> Final=new ArrayList<String>();
+		int start_index=0;
+		int End_index=0;
+		int i=0;
+		while(litr.hasNext()){
+			System.out.println(litr.next());
+			if(litr.equals("(")){
+				start_index=litr.nextIndex();
+				System.out.println("( entered"+start_index);
+			}
+			if(litr.equals(")")){
+				End_index=litr.previousIndex();
+				System.out.println("( entered"+End_index);
+				Final.add(i,Exp.get(start_index));
+				Final.add(i+1,Exp.get(End_index));
+				Final.add(i+2,Exp.get(End_index-1));
+				i=i+3;
+				Exp.subList(start_index-1, End_index+1).clear();
+				Exp.add(start_index-1, Arrays.toString(Final.toArray()));
+				System.out.println(Final);
+				System.out.println(Exp);
+				litr=Exp.listIterator();
+			}
+		}
 		return converted;
 	}
 }
