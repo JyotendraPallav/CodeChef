ba7b33124101a4c19b41ff2cc3f330b5cb8919c9
JyotendraPallav <Jyotendra@gmail.com>
2016-12-25 16:41:07 +0530

    Transform: Trying out

--- src/Transform.java
+++ src/Transform.java
@@ -8,14 +8,24 @@ public class Transform {
 			if(sc.hasNext()){
 				int No_Tests=sc.nextInt();
 				List<String> Expr=new ArrayList<String>();
-				for(int i=1;i<=No_Tests;i++){
+				List<String> Expr_Converted=new ArrayList<String>();
+				for(int i=0;i<No_Tests;i++){
 					Expr.add(sc.next());
+					Expr_Converted.add(Transform_exp(Expr.get(i)));
 				}
-				System.out.println(Expr);
+				//System.out.println(Expr);
+				//Expr_Converted.forEach(System.out::println);
 			}
 		}finally{
 			sc.close();
-		}				
+		}				 
 	}
 	
+	static String Transform_exp(String Expression){
+		String converted="";
+		List<String> Exp=new ArrayList<String>(Arrays.asList(Expression.split("")));
+		System.out.println(Exp.subList(0, Exp.indexOf(")")));
+		System.out.println(Exp);
+		return converted;
+	}
 }
