ba2af4185cd2ce87a7b421fe51d17ee0eb46778a
JyotendraPallav <Jyotendra@gmail.com>
2017-01-12 18:27:42 +0530

    Add Numbers: Completed  and submitted

--- src/Add_Numbers.java
+++ src/Add_Numbers.java
@@ -5,7 +5,7 @@ class Add_Numbers {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("To Add Two numbers: \n");
+		//System.out.println("To Add Two numbers: \n");
 		try{
 			if(sc.hasNextInt()){
 				int iters=sc.nextInt();
@@ -13,18 +13,28 @@ class Add_Numbers {
 				List<Integer> num_sum=new ArrayList<Integer>();
 				for(int i=0;i<iters;i++){
 					numbers.add(sc.nextLong());
-					num_sum.add(Add_Number(Long.toString(numbers.get(i))));
+					List<String> Value_Str=new ArrayList<String>(Arrays.asList(Long.toString(numbers.get(i)).split("")));
+					num_sum.add(Add_Number(Value_Str));
 				}
+				num_sum.forEach(System.out::println);
 			}
 		}finally{
 			sc.close();
 		}
 	}
 
-	static int Add_Number(String Value){
+	static int Add_Number(List<String> Value){
 		int sum=0;
-		List<String> Value_Str=new ArrayList<String>(Arrays.asList(Value.split("")));
-		System.out.println(Value_Str);
+		if(Value.size()>0){
+			sum+=Integer.valueOf(Value.get(0));
+			//System.out.println(sum);
+			Value=Value.subList(1,Value.size());
+			//System.out.println(Value);
+			sum+=Add_Number(Value);
+		}
+		//System.out.println(sum);
+		//System.out.println(Value);
 		return sum;
+
 	}
 }
